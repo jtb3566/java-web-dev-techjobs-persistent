35e3546b1caa226e081a78cb31b4c1ae47ca9e4c
James Ballard <ballardj77@gmail.com>
2022-02-10 14:52:46 -0600

    completed task#1 techjobs app connects with techjobs table in mySQL

--- queries.sql
+++ queries.sql
@@ -1,5 +1,9 @@
 ## Part 1: Test it with SQL
-
+    columns:
+        id int PK
+        employer varchar(255)
+        name varchar(255)
+        skills varchar(255)
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,23 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "location is required")
+    @Size(min = 3, max = 50, message = "Location should be between 3 and 50 characters")
+    private String location;
 
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
