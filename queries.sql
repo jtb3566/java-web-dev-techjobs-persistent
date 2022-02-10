e77e099ccd769188ea1e784b5753bd6ec6b2e309
James Ballard <ballardj77@gmail.com>
2022-02-10 15:22:10 -0600

    Completed EmployerController and SkillController TechJobs app properly adds new employers and skills to the database, displayed them, and displays their details

--- queries.sql
+++ queries.sql
@@ -5,6 +5,7 @@
         name varchar(255)
         skills varchar(255)
 ## Part 2: Test it with SQL
+SELECT name FROM techjobs.employer WHERE location = "St. Louis City"
 
 ## Part 3: Test it with SQL
 
